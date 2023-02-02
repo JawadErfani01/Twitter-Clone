52952a7396e32b7749511e60682037b8b3b1e5f2
OmarsSedigh <omarssedig@gmail.com>
2023-02-02 22:33:30 +0430

    design of sidebar  twitter-clone completed

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="col-span-12 mx-4 my-2">
+      <h1 className="text-[22px] font-semibold ">Home</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchBar = () => {
+  return <div className="bg-blue-300  col-span-7">SearchBar</div>;
+};
+
+export default SearchBar;
